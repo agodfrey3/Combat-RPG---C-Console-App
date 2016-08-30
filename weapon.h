6a7b813ac72efb903656edd3ee0ca94c9722d9fe
agodfrey3 <agodfrey3@fordham.edu>
2016-08-29 22:27:08 -0400

    Updated to work with new equip system

--- weapon.h
+++ weapon.h
@@ -2,8 +2,14 @@ class weapon : public item{
 
 	public:
 		int damage;
+
 		int getDamage();
-		weapon(string itemName, int wepDamage) :item(itemName, wepDamage){};
+		weapon(string itemName, int wepDamage) :item(itemName, wepDamage)
+		{
+			useVal = 0;
+			id     = itemName;
+			stat   = wepDamage;
+		};
 		string getStat();
 };
 int weapon::getDamage()
